304cdb44d967386761d3218b0f66609be87a481e
Johann Mayos Lopes da Silva <42842871+JohannMayos@users.noreply.github.com>
2022-08-25 22:14:30 -0400

    hash function added

--- hashFunction.c
+++ hashFunction.c
@@ -1,8 +1,8 @@
 #include "chestLib.h"
 
 int hashFunction(int key) {
-    /* 
-     * A função hash deve retornar a posição da tabela na qual o item será
-     * inserido, ou seja, um número entre 0 e 30
-     */
+    
+    return key % TABLE_SIZE;
+    
+    
 }
