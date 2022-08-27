0e72a67ff026a38401814572cd8aca9e0a4f3961
Carlos Eduardo da Silva Martins <cedsm.snf20@dmz.org.br>
2022-08-27 13:24:23 -0400

    Search implemented

--- chestLib.h
+++ chestLib.h
@@ -6,37 +6,48 @@
 #define NAME_SIZE 100
 #define TABLE_SIZE 31
 
-typedef struct Item {
+typedef struct Item
+{
     char name[NAME_SIZE];
     int quantity;
     int key;
 } Item;
 
-typedef struct Node {
-    Item * item;
-    struct Node * next;
+typedef struct Result
+{
+    Item item;
+    int position;
+    bool isAlone;
+} Result;
+
+typedef struct Node
+{
+    Item *item;
+    struct Node *next;
 } Node;
 
-typedef struct Slot {
-    Node * firstNode;
+typedef struct Slot
+{
+    Node *firstNode;
 } Slot;
 
-typedef struct Chest {
-    Slot * slots[TABLE_SIZE];
+typedef struct Chest
+{
+    Slot *slots[TABLE_SIZE];
     int busySlots;
 } Chest;
 
 // Utils functions
-Item * createItem(char itemName[NAME_SIZE], int itemQuantity, int itemKey);
-Node * createNode(Item * item);
-bool isChestFull(Chest * chest);
-void initializeChest(Chest * chest);
+Item *createItem(char itemName[NAME_SIZE], int itemQuantity, int itemKey);
+Node *createNode(Item *item);
+bool isChestFull(Chest *chest);
+void initializeChest(Chest *chest);
 
 // Main functions
 int hashFunction(int key, char *name);
-int handleCollision(Chest * chest, Node * newNode, int pos);
-int insertNode(Chest * chest, Item * item, int pos);
-void removeNode(Chest * chest, char itemName[NAME_SIZE], int pos);
-int searchNode(Chest * chest, char itemName[NAME_SIZE], int pos);
+int handleCollision(Chest *chest, Node *newNode, int pos);
+int insertNode(Chest *chest, Item *item, int pos);
+void removeNode(Chest *chest, char itemName[NAME_SIZE], int pos);
+Result searchNode(Chest *chest, char itemName[NAME_SIZE], int pos);
 
 #endif
