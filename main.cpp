9008dbdae872ca3cfe7934b58b96887315a5b170
Daniela <feitosaferreiradaniela@gmail.com>
2022-08-27 15:29:18 -0400

    define a unique main

--- main.cpp
+++ main.cpp
@@ -1,11 +1,17 @@
 #include "interface/interfaceLib.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "chestLib.h"
 
 int opt = 0, page = HOME;
 telaInserir inserir;
 telaRemover remover;
 int lista[31];
 
-int main() {
+int main(int argc, const char * argv[]) {
+    Chest * chest = (Chest *) malloc(sizeof(Chest));
+    initializeChest(chest);
+    printf("%d\n", hashFunction(256, "PICKAXE"));
 
 	char tecla = 0;
 	createListaObjetos();
