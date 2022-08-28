caf7e1fa4f1a61c5ee50f23c209676c6ee8be83d
ronaldocosta <ronaldocosta.developer@gmail.com>
2022-08-28 13:42:05 -0400

    Change the search function return

--- search.c
+++ search.c
@@ -1,25 +1,18 @@
 #include "chestLib.h"
 
-struct Result searchNode(Chest *chest, char itemName[NAME_SIZE], int pos)
+Node * searchNode(Chest * chest, char itemName[NAME_SIZE], int pos)
 {
-  Result result;
-  result.position = pos;
-  result.isAlone = true;
-
-  Node *aux = chest->slots[pos]->firstNode;
+  Node * result = NULL, * aux = chest->slots[pos]->firstNode;
 
   while (aux != NULL)
   {
-    if (strcmp(aux->item->name, itemName) != 0)
-      result.item = *aux->item;
-    else
-      result.isAlone = false;
-
+    if (strcmp(aux->item->name, itemName) != 0) 
+    {
+      result->item = aux->item;
+      return result;
+    }
     aux = aux->next;
   }
 
-  if (result.item.key)
-    return result;
-
-  return result; //corrigir esse retorno para equivalente a NULL
+  return result;
 }
